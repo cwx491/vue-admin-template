8c19a37316723bf75e520a9bc5afb412d3422927
cwx491 <604298674@qq.com>
2020-04-07 22:56:55 +0800

    用户登录

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -34,6 +34,7 @@ const actions = {
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
+        console.log(response,data)
         commit('SET_TOKEN', data.token)
         setToken(data.token)
         resolve()
